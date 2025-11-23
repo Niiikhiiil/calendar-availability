853fcfbb93612230f718610f76a041e61aff5ce8
Niiikhiiil <nikhilmuliya21@gmail.com>
2025-11-23 22:07:35 +0530

    baseurl changed

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export const base = "http://localhost:4000";
-// export const base = "https://calendar-availability-backend.onrender.com";
+// export const base = "http://localhost:4000";
+export const base = "https://calendar-availability-backend.onrender.com";
 
-const baseUrl = "http://localhost:4000/api";
-// const baseUrl = "https://calendar-availability-backend.onrender.com/api";
+// const baseUrl = "http://localhost:4000/api";
+const baseUrl = "https://calendar-availability-backend.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
